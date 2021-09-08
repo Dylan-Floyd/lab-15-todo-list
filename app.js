c5971b40e813869120dfcb3289409b59b47d20e9
Dylan Floyd <floyd800@gmail.com>
2021-09-08 14:19:38 -0700

    uh oh i broke something in git

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+import { getUser } from './local-storage-utils.js';
+
 // import functions and grab DOM elements
-
-
+const form = document.querySelector('#login-signup-form');
+const loginButton = document.querySelector('#login-button');
+const signupButton = document.querySelector('#signup-button');
+const retryDisplay = document.querySelector('input.invalid-creds');
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+loginButton.addEventListener('click', () => {
+    const user = getUser();
+    const formData = new FormData(form);
+    const enteredUsername = formData.get('username');
+    const enteredPassword = formData.get('password');
+
+    if (user.username === enteredUsername && user.password === enteredPassword) {
+        window.location = './todos/';
+    } else {
+        retryDisplay.classList.remove('hidden');
+    }
+});
